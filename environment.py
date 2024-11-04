8447f95a08fb77ae5699fcfdea4f26a35a1eadb3
bohuinong <81490819+bohuinong@users.noreply.github.com>
2024-11-04 15:43:50 -0500

    connected vision and socket to environment

--- environment.py
+++ environment.py
@@ -3,26 +3,39 @@ from gymnasium import spaces
 import numpy as np
 from pynput.keyboard import Controller
 from concurrent.futures import ThreadPoolExecutor
+from helper import SocketListener
 
 class OsuEnvironment(gym.env):
     def __init__(self):
         # the keys corresponading to the 4 lane
         self.keys = ['s', 'd', 'k', 'l']
+
         # dyanamic list of vectors return tuple of (type, lane, y position) 
         # placehold for fixed size as 20
         self.observation = spaces.Box(low=0, high=np.lnf, shape=(20, 3), dtype=np.float32)
+
         # 4 lane where each note can do nothing, pressed, held or release
         self.action_space = spaces.MultiDiscrete([4]*len(self.keys))
+        
         # keep track of which key is hold
         self.currently_hold = [False] * len(self.keys)
+
         # check for invalid key press
         self.invalid = False
         self.keyboard = Controller()
+
+        # socket setup
+        self.data_queue = []
+        self.listener = SocketListener(data_handler=self.data_handler)
+        self.socket_executor = ThreadPoolExecutor(max_workers=8)
+        self.socket_executor.submit(self.check_connection)
+        self.socket_executor.submit(self.listener.start)
 
     def reset(self):
         self.currently_hold = [False] * len(self.keys)
         self.observation = np.empty()
         self.invalid = False
+
         return self.observation
     
     def keyboard_action(self, key, action):
@@ -40,18 +53,24 @@ class OsuEnvironment(gym.env):
                     self.keyboard.release(key)
                 else:
                     self.invalid = True
+    
+    def data_handler(self, data):
+        self.data_queue.append(data)
 
-    def step(self, actions):
-        # take action based on the given actions simultaneously
-        with ThreadPoolExecutor(max_workers=len(self.keys)) as executor:
-            for lane in range(actions):
-                executor.submit(self.keyboard_action(self.keys[lane], actions[lane]))
-        
-        # some method to get socket
-        socket = getsocket()
+    def check_connection(self):
+        while True:
+            if not self.listener.is_first_connection and not self.listener.has_connection:
+                self.listener.stop()
+                break
+
+    def get_reward(self):
+        reward = 0
+        # method to get hit type
+        data = self.data_queue.pop(0)
+        hit_type = int.from_bytes(data, byteorder='little')
 
         # reward based on the action taken
-        match socket:
+        match hit_type:
             case 0: # miss
                 reward = -3
             case 1: # meh
@@ -68,11 +87,22 @@ class OsuEnvironment(gym.env):
         # if there are invalid action
         if self.invalid:
             reward += -10
+            self.invalid = False
 
-        self.invalid = False
+        return reward
+    
+    def set_observation(self, obs):
+        self.observation = obs
 
-        # some method to get the notes vector, truncate and terminate state
-        self.observation = getNotes()
+    
+    def step(self, actions):
+        # take action based on the given actions simultaneously
+        with ThreadPoolExecutor(max_workers=len(self.keys)) as executor:
+            for lane in range(actions):
+                executor.submit(self.keyboard_action(self.keys[lane], actions[lane]))
+        
+        # methods to get reward, truncate and terminate state
+        reward = self.get_reward()
         truncate = getTruncate()
         terminate = getTerminate()
         
